1a5a096b14749ab21a0686fe3cabb3c16e89a173
Yale Araujo <yale850@gmail.com>
2023-02-23 11:41:45 -0300

    last commit

--- src/components/CallToAction/index.tsx
+++ src/components/CallToAction/index.tsx
@@ -1,7 +1,14 @@
 export function CallToAction({ isCenter = false }) {
+
+    function handleGoesToForm() {
+        window.location.href = 'https://form.respondi.app/968zaZgk'
+    }
+
     return (
         <button
-            className={`bg-highlight text-md font-bold text-black ${isCenter ? 'w-[750px]' : 'w-full'} mobile:w-full block mx-auto mt-8 p-4 rounded-md mobile:text-sm mobile:p-2 mobile:mt-6 animate-bounce`}>
+            onClick={handleGoesToForm}
+            className={`bg-highlight text-md font-bold text-black ${isCenter ? 'w-[750px]' : 'w-full'}
+            mobile:w-full block mx-auto mt-8 p-4 rounded-md mobile:text-sm mobile:p-2 mobile:mt-6 animate-bounce hover:bg-gradient-to-r from-orange-300 to-orange-500`}>
             Quero ser selecionado para a Sessão Estratégica
         </button>
     )
