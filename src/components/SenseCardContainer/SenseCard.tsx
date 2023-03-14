755992be22d3b4581d2eb5e705448c7ad528f52c
Yale Araujo <yale850@gmail.com>
2023-03-14 20:00:07 -0300

    feat: images senses section optimization

--- src/components/SenseCardContainer/SenseCard.tsx
+++ src/components/SenseCardContainer/SenseCard.tsx
@@ -4,7 +4,7 @@ export function SenseCard({image, alternativeText, thumb}) {
     return (
         <div className="w-80 h-44 rounded-xl relative shadow-md transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110">
             <h3 className='text-white absolute font-bold text-xl max-w-sm text-center top-12 z-10'>{thumb}</h3>
-            <Image src={image} alt={alternativeText} width={320} height={176} className='w-full h-full object-cover opacity-20 rounded-xl' priority/>
+            <Image src={image} alt={alternativeText} width={320} height={176} placeholder="blur" className='w-full h-full object-cover opacity-20 rounded-xl' priority/>
         </div>
     )
 }
