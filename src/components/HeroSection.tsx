db4fdcfb193e2ce3ffb76910122194dbf0ead04f
Yale Araujo <yale850@gmail.com>
2023-03-14 19:47:00 -0300

    feat: images hero section optimization

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -8,14 +8,16 @@ export function HeroSection() {
     return (
         <main className="relative border-gray-900 py-16 mobile:py-4 bg-[url('../../public/background.png')] bg-cover mobile:bg-none">
             <div className='w-[400px] mx-auto mobile:w-64'>
-                <Image src={Logo} alt='Logo Matheus Fernandes Nutri' width={400} height={400} />
+                <Image src={Logo} alt='Logo Matheus Fernandes Nutri' width={400} height={400} placeholder='blur' />
             </div>
 
             <div className='max-w-7xl w-full flex items-center justify-between mx-auto mt-4 mobile:max-w-[350px] mobile:flex-col' >
                 <div className='w-[700px] mobile:w-96'>
                     <Image
                         src={HeroImage}
+                        width={700} height={700}
                         alt='Nutricionista Matheus Fernandes cruzando os braços'
+                        placeholder='blur'
                     />
                 </div>
 
